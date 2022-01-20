54f87cc6c03fef412d42b93779959c11431ab089
vignol <89270861+vignol@users.noreply.github.com>
2022-01-20 19:58:54 +0100

    mail communique

--- app/Mail/AllRespoMail.php
+++ app/Mail/AllRespoMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class UserMail extends Mailable
+class AllRespoMail extends Mailable
 {
     use Queueable, SerializesModels;
     public $user;
